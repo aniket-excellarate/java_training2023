5bef42ad23c6cbcbd46f2b22d4a53f698bffe0ae
aniket-excellarate <aniket.mane@excellarate.com>
2023-05-16 16:17:50 +0530

    added

---
+++ java/MyFirstApp/src/Entry.java
@@ -0,0 +1,11 @@
+import java.awt.Frame;
+
+public class Entry {
+	
+	public static void main(String[] args) {
+		
+		MyFrame f = new MyFrame(true, "My First Window", 100, 350, 400, 200);
+		
+		//Frame f1 = new Frame();
+	}
+}
